2a1a93225795fed9c7529c8f4c075b90aa661391
Alan Veloso <alantsv@gmail.com>
2017-10-05 19:20:35 -0300

    Create atividade01-comandos_select_em_sql.sql

---
+++ atividade01-comandos_select_em_sql.sql
@@ -0,0 +1,59 @@
+/*
+a) Faça uma seleção que mostre sobrenome (last_name) dos empregados que possuem a
+terceira letra sendo “a”. Nesta seleção mostre também o nome do departamento
+(department_name) que o empregado trabalha, ordenando a saída de dados pelo último
+nome do empregado (last_name) e ordem alfabética ascendente. Usar os operadores
+LIKE e ORDER BY.
+*/
+select last_name, department_name 
+from employees emp, departments dept
+where last_name like '__a%' and emp.department_id = dept.department_id 
+order by last_name;
+
+/*
+b) Construa uma query (consulta) que mostre a quantidade de empregados agrupados por
+nome do departamento (department_name). Usar os operadores COUNT e GROUP BY.
+*/
+select count(*), department_name 
+from employees emp, departments dept 
+where emp.department_id = dept.department_id 
+group by department_name;
+
+/*
+c) Escreva uma query (consulta) que mostre a diferença entre o maior e o menor salário
+(salary) da tabela de empregados (Employee). Faça com que o resultado seja mostrado
+com o label “Diferença”. Usar as funções de agregação MAX e MIN.
+*/
+select max(salary) - min(salary) 
+as Diference
+from employees;
+
+/*
+d) Mostrar a quantidade de empregados agrupados por nome da região (region_name).
+Usar os operadores COUNT e DISTINCT.
+*/
+select region_name, count(employee_id)
+from regions reg, employees emp, departments dep, locations loc, countries
+where emp.department_id = dep.department_id and dep.location_id = loc.location_id and loc.country_id = countries.country_id and countries.region_id = reg.region_id
+group by region_name;
+
+/*
+e) Escreva uma consulta que mostre o nome do departamento (department_name),
+localização do mesmo (street_address), quantidade de funcionários e média de salários.
+Agrupar o resultado por nome do departamento (department_name) e localização do
+mesmo (street_address).
+*/
+select department_name, street_address, count(employee_id), avg(salary)
+from departments dep, employees emp, locations loc
+where dep.department_id = emp.department_id and dep.location_id = loc.location_id
+group by department_name, street_address;
+
+/*
+g) Para cada empregado mostre o número do mesmo (employee_id), sobrenome
+(last_name), salário (salary) e o salário acrescido do valor do menor salário (min_salary)
+para o cargo que ele exerce. Faça com que o salário acrescido seja mostrado com o label
+“Novo Salário”.
+*/
+select employee_id, last_name, salary, salary + min(salary) as Novo_Salario
+from employees
+group by employee_id, last_name, salary;
